072723975c2477d79a67a3a4e6e976cfd71c521c
eevitg <vketh@yandex.ru>
2023-12-20 14:07:19 +0300

    add form

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 
 export default function Form() {
+  let comments =[]
+  const [comment, setComment] = useState('')
 
-
+  function addComment() {
+    comments.push(comment);
+    console.log(comments)
+    setComment('')
+    return comments;
+  }
 
 
   return (
     <form>
         <h1>chat</h1>
+        <div>{comment}</div>
         <h2>your comment</h2>
 <input type='text' required value={comment} onChange={(e) => setComment(e.target.value)} className='comment__input' placeholder="your comment" name="comment"></input>
+<button type='button' onClick={addComment}>send</button>
     </form>
   )
 }
