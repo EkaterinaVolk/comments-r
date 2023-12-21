278bfabba08b2f122fe5de2ae2960fd2bae79de5
eevitg <vketh@yandex.ru>
2023-12-21 11:12:25 +0300

    add check spam

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-// import { useState } from 'react'
 import Form from './Form'
 import './App.css'
-
-function App() {
-
+ 
+const App = () => {
+ 
   return (
-      <div>
-        <Form/>
-      </div>
-
-  )
-}
-
-export default App
+<Form/>
+  );
+};
+ 
+export default App;
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,24 +1,27 @@
-import { useState } from 'react'
+import './Form.css'
+import {useState} from 'react'
+let listOfComments = []
 
 export default function Form() {
-  let comments =[]
-  const [comment, setComment] = useState('')
+	const [items, setItems] = useState("comment", []);
+ 
+  const handleItems = () => {
+    setItems(items => listOfComments.push(checkSpam(items)))
 
-  function addComment() {
-    comments.push(comment);
-    console.log(comments)
-    setComment('')
-    return comments;
-  }
-
+	}
+ 
+  function checkSpam(str) {
+    str = str.replace(/viagra/gi, '***').replace(/xxx/gi, '***');
+    return(str);
+    }
 
   return (
-    <form>
+        <form>
         <h1>chat</h1>
-        <div>{comment}</div>
+        <div className='comments'><ul> {listOfComments.map((comment) => <li>{comment}</li>)}</ul></div>
         <h2>your comment</h2>
-<input type='text' required value={comment} onChange={(e) => setComment(e.target.value)} className='comment__input' placeholder="your comment" name="comment"></input>
-<button type='button' onClick={addComment}>send</button>
+<input type='text' required onChange={(e) => setItems(e.target.value)} className='comment__input' placeholder="your comment" name="comment"></input>
+<button type='button' onClick={handleItems}>send</button>
     </form>
-  )
+    );
 }
