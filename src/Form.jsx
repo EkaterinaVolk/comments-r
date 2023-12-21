58fcf166e8a096b4f2d07fb6f2f353cc159c71c0
eevitg <vketh@yandex.ru>
2023-12-21 18:10:09 +0300

    add add comment function

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,15 +1,16 @@
 import './Form.css'
 import {useState} from 'react'
-let listOfComments = []
+
+// не смогла придумать, как сделать новый комент другого цвета... даже идей нет, куда двигаться
 
 export default function Form() {
-	const [items, setItems] = useState("comment", []);
- 
-  const handleItems = () => {
-    setItems(items => listOfComments.push(checkSpam(items)))
-    setItems('')
-
-	}
+  const [comment, setComment] = useState('');
+	const [listOfComments, setListOfComments] = useState([]);
+   
+  function addComment () {
+    setListOfComments([checkSpam(comment), ...listOfComments])
+    setComment('')
+  }
  
   function checkSpam(str) {
     str = str.replace(/viagra/gi, '***').replace(/xxx/gi, '***');
@@ -19,10 +20,10 @@ export default function Form() {
   return (
         <form>
         <h1>chat</h1>
-        <div className='comments'><ul> {listOfComments.map((comment) => {return <li>{comment} </li>})}</ul></div>
-        <h2>your comment</h2>
-<input type='text' required value={items} onChange={(e) => setItems(e.target.value)} className='comment__input' placeholder="your comment" name="comment"></input>
-<button type='button' onClick={handleItems}>send</button>
+        <div className='comments'><div> {listOfComments.map((comment) => <div> {comment} </div>)}</div></div>
+        <h2>your comment:</h2>
+<input type='text' required value={comment} onChange={(e) => setComment(e.target.value)} className='comment__input' placeholder="your comment" name="comment"></input>
+<button type='button' onClick={addComment}>send</button>
     </form>
     );
 }
